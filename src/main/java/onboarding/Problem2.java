a11e2e7f41174cb578db7741b4ad2b99f10845b6
LocKey <rudfhr97@naver.com>
2024-09-01 03:39:03 +0900

    문제 2 해결

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,21 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer;
+
+        while(true) {
+            int count = 0;
+            for (int i = 0; i < cryptogram.length() - 1; i++) {
+                if (cryptogram.charAt(i) == cryptogram.charAt(i + 1)) {
+                    cryptogram = cryptogram.substring(0, i) + cryptogram.substring(i + 2);
+                    i--;
+                    count++;
+                }
+            }
+            if(count == 0) break;
+        }
+
+        answer = cryptogram;
         return answer;
     }
 }
