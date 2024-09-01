4e66704ce082938156599f78e933ab966556c953
LocKey <rudfhr97@naver.com>
2024-09-01 17:07:08 +0900

    문제 6 해결

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,40 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
+
+        for(int i = 0; i < forms.size(); i++) {
+            String name = forms.get(i).get(1);
+            String email = forms.get(i).get(0);
+
+            if(answer.contains(email)) continue;
+
+            for(int j = i + 1; j < forms.size(); j++) {
+                String compName = forms.get(j).get(1);
+                String compEmail = forms.get(j).get(0);
+
+                if(answer.contains(compEmail)) continue;
+
+                for(int k = 0; k < compName.length() - 1; k++) {
+                    String subString = compName.substring(k, k + 2);
+                    if(name.contains(subString)) {
+                        answer.add(forms.get(j).get(0));
+                        if(!answer.contains(email)) {
+                            answer.add(forms.get(i).get(0));
+                        }
+                        break;
+                    }
+                }
+
+            }
+        }
+        answer.sort(Comparator.naturalOrder());
+
         return answer;
     }
 }
