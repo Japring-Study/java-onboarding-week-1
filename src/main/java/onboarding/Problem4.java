1eb140ead2d33312adfab3701f8dcd292ff9cc66
LocKey <rudfhr97@naver.com>
2024-09-01 15:05:58 +0900

    문제 4 해결

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,24 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        for(int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+
+            if(c == ' ') {
+                answer += ' ';
+                continue;
+            }
+
+            char newChar;
+            if(c >= 97) { //소문자
+                newChar = (char) (219 - c);
+            } else { // 대문자
+                newChar = (char) (155 - c);
+            }
+            answer += newChar;
+        }
+
         return answer;
     }
 }
