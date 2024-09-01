b14b253df579271431c1a0677c5d504891be8554
LocKey <rudfhr97@naver.com>
2024-09-01 14:35:48 +0900

    문제 3 해결

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,19 @@
 package onboarding;
 
+import java.util.List;
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for(int i = 1; i <= number; i++) {
+            List<Integer> digits = Problem1.splitDigit(i);
+            for(int j = 0; j < digits.size(); j++) {
+                if(digits.get(j) == 3 || digits.get(j) == 6 || digits.get(j) == 9) {
+                    answer++;
+                }
+            }
+        }
+
         return answer;
     }
 }
