6c8e2ba1412d033c295dfafc4dc2c44a9f5ceac2
LocKey <rudfhr97@naver.com>
2024-09-01 15:44:01 +0900

    문제 5 해결

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,28 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        List<Integer> moneyUnit = new ArrayList<>() {{
+            add(50000);
+            add(10000);
+            add(5000);
+            add(1000);
+            add(500);
+            add(100);
+            add(50);
+            add(10);
+            add(1);
+        }};
+
+        for(int i = 0; i < moneyUnit.size(); i++) {
+            answer.add(money / moneyUnit.get(i));
+            money = money % moneyUnit.get(i);
+        }
         return answer;
     }
 }
