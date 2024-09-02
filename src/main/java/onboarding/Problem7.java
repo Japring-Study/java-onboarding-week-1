880635cedf12787ef1e72af889708b77c14207df
yohanii <kim-yohan@gim-yohan-ui-MacBookPro-2.local>
2024-09-03 08:33:21 +0900

    feat: Problem7 사용자별 친구 리스트를 저장한 맵 반환 - getFriends()

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,30 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    private static Map<String, List<String>> getFriends(List<List<String>> friends) {
+        Map<String, List<String>> friendsPerUser = new HashMap<>();
+
+        for (List<String> friend : friends) {
+            String user1 = friend.get(0);
+            String user2 = friend.get(1);
+
+            if (!friendsPerUser.containsKey(user1)) {
+                friendsPerUser.put(user1, new ArrayList<>());
+            }
+            if (!friendsPerUser.containsKey(user2)) {
+                friendsPerUser.put(user2, new ArrayList<>());
+            }
+            friendsPerUser.get(user1).add(user2);
+            friendsPerUser.get(user2).add(user1);
+        }
+
+        return friendsPerUser;
     }
 }
