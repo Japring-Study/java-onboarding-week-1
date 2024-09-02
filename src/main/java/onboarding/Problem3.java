6e1c17f99af5d6b738cff22e556045a3e67dbc85
yohanii <kim-yohan@gim-yohan-ui-MacBookPro-2.local>
2024-09-02 19:15:35 +0900

    feat: Problem3 1 ~ num (입력받은 수)까지 각 수마다 박수 값을 구해서 배열 반환 - countClap()

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,26 @@
 package onboarding;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Problem3 {
+
+    private final static List<Integer> special = List.of(3, 6, 9);
+
     public static int solution(int number) {
         int answer = 0;
         return answer;
+    }
+
+    private static int[] countClap(int last) {
+
+        int[] count = new int[last + 1];
+        for (int num = 1; num <= last; num++) {
+
+            int lastDigitCount = special.contains(num % 10) ? 1 : 0;
+            count[num] = count[num/10] + lastDigitCount;
+        }
+
+        return count;
     }
 }
