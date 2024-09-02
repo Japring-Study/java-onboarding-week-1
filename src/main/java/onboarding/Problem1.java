6f4399e07bd031237390c7bd32d5ede75433902c
yohanii <kim-yohan@gim-yohan-ui-MacBookPro-2.local>
2024-09-02 09:07:23 +0900

    feat: 숫자를 입력받아, 각 자리수의 합, 곱 중 큰 수를 반환하는 함수 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,40 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
+    }
+
+    private static int getMaxOfDigitSumAndMultiply(int num) {
+
+        List<Integer> digits = getDigits(num);
+
+        Integer sum = digits.stream()
+                .reduce(0, Integer::sum);
+        Integer multiply = digits.stream()
+                .reduce(1, (x, y) -> x * y);
+
+        return Integer.max(sum, multiply);
+    }
+
+    private static List<Integer> getDigits(int num) {
+
+        List<Integer> digits = new ArrayList<>();
+
+        int cur = num;
+        while (cur > 0) {
+            digits.add(0, cur % 10);
+            cur /= 10;
+        }
+
+        if (digits.isEmpty()) {
+            digits.add(0);
+        }
+
+        return digits;
     }
 }
