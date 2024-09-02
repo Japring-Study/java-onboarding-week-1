a5d38fed9b809c73529154adb4572d4d6a540831
yohanii <kim-yohan@gim-yohan-ui-MacBookPro-2.local>
2024-09-02 13:58:06 +0900

    feat: Problem1 검증 로직 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -2,9 +2,14 @@ package onboarding;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+
+        if (!isValid(pobi) || !isValid(crong)) {
+            return -1;
+        }
 
         int pobiScore;
         int crongScore;
@@ -26,11 +31,15 @@ class Problem1 {
         return 0;
     }
 
+    private static boolean isValid(List<Integer> pages) {
+        return pages.size() == 2 && pages.get(0) == pages.get(1) - 1;
+    }
+
     private static int getScore(List<Integer> pages) {
         return pages.stream()
                 .map(Problem1::getMaxOfDigitSumAndMultiply)
                 .max(Integer::compareTo)
-                .orElseThrow(() -> new IllegalStateException("동작 오류"));
+                .orElseThrow(() -> new NoSuchElementException("동작 오류"));
     }
 
     private static int getMaxOfDigitSumAndMultiply(int num) {
