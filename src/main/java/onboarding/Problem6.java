e759108caf9b4db82233fbd1d8068508e691196f
yohanii <kim-yohan@gim-yohan-ui-MacBookPro-2.local>
2024-09-02 20:29:12 +0900

    feat: Problem6 forms를 받아, 겹치는 닉네임을 가진 사람 이메일 Set을 반환 - getDuplicated()

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,13 +1,32 @@
 package onboarding;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
+    }
+
+    private static Set<String> getDuplicated(List<List<String>> forms) {
+
+        Set<String> duplicated = new HashSet<>();
+
+        Map<String, Set<String>> emailsPerTwoLetter = new HashMap<>();
+        for (List<String> form : forms) {
+            String email = form.get(0);
+            String nickName = form.get(1);
+
+            for (String letter : getTwoLetters(nickName)) {
+                if (!emailsPerTwoLetter.containsKey(letter)) {
+                    emailsPerTwoLetter.put(letter, new HashSet<>(){{add(email);}});
+                    continue;
+                }
+                emailsPerTwoLetter.get(letter).add(email);
+                duplicated.addAll(emailsPerTwoLetter.get(letter));
+            }
+        }
+        return duplicated;
     }
 
     private static Set<String> getTwoLetters(String nickName) {
