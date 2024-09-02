940147b74729fa0503afa023915b8ec0528b6ad1
msung99 <msung6924@naver.com>
2024-09-02 20:38:16 +0900

    feat: 2번 해결

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,26 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : cryptogram.toCharArray()) {
+            if (!stack.isEmpty() && stack.peek() == c) {
+                stack.pop();  // 스택의 최상단 요소 제거
+            } else {
+                stack.push(c);
+            }
+        }
+
+        // 스택에 남은 문자들을 합쳐 최종 문자열 반환
+        StringBuilder result = new StringBuilder();
+        for (char c : stack) {
+            result.append(c);
+        }
+
+        return result.toString();
     }
+
 }
